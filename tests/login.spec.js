d4b6180e21a05af19c975d0e5b2f1f2fd220f109
soundarya <soundarya.arikati@miele.com>
2024-09-09 16:42:56 +0200

    test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -2,7 +2,7 @@ import { test ,expect } from '@playwright/test'
 import pageManager from '../utils/pageManager'
 
 
-//testghfg
+
 let createPage
 test('valid login', async ({ page }) => {
     createPage= new pageManager(page)
